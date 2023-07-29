b0b301829f9507bb4b758b8a3fc2474817221692
buffer <714433615@qq.com>
2023-07-29 16:10:14 +0800

    feat:del monolog/monolog

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,17 @@
 
 use App\Http\Controller\IndexController;
 use App\Library\Route\Route;
+use App\Model\User;
 
 global $app;
 
 $route=$app->make(Route::class);
 
 $route->get('/', function () {
-    return  'api/';
+    $id = 1;
+   return (new User())->select('uid', 'Agent', 'Account')->where('uid', $id)->with(['agent' => function ($q) {
+        $q->select('Account', 'aid');
+    }])->first();
 });
 
 $route->get('/home',[IndexController::class,'index'])->middleware('UserAuth');
